1d49f2fbd3c77761d6518e40d632a5c10547f7ba
Prateek Nischal <PNischal@walmartlabs.com>
2018-03-12 10:58:50 +0530

    FIX: syntax:

--- client.sh
+++ client.sh
@@ -1,8 +1,9 @@
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./client.sh <server_IP_address>"
+  exit -1
+fi
+
 rm client_pipe
 mkfifo client_pipe
-function() {
-  if [ "$#" -ne 1 ]; then
-    echo "usage: ./client.sh <server_IP_address>"
-    exit -1 
-}
+
 openssl s_client -connect $1:4433 -brief -nbio 2>/dev/null 1>client_pipe
